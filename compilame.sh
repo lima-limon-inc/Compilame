35b0edb4e6b23dcb9dd18bf1484047f36a70bdda
Fabri <moboficial00@gmail.com>
2022-10-04 23:57:02 -0300

    Chequeo de que le pases un archivo assmebly

--- compilame.sh
+++ compilame.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+if [ ".asm" != $(echo -n $1 | tail -c 4) ] && [ ".s" != $(echo -n $1 | tail -c 2) ]
+then
+	echo '''
+Tenes que pasarme un archivo ".asm" o ".s"
+	'''
+	exit 2 #Paso 2 como error porque el usuario no paso un archivo existente
+fi
+
 echo "Compilo el archivo asembly a objeto"
