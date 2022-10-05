e4359b8d3bcc2fe4c0721ef8293722e917887c66
Fabri <moboficial00@gmail.com>
2022-10-05 00:02:47 -0300

    Chequeo de que el archivo pasado existe

--- compilame.sh
+++ compilame.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+if [ ! -f "$1" ]
+then
+	echo "
+${1} no existe
+	"
+	exit 2 #Paso 2 como error porque el usuario no paso un archivo existente
+fi
+
 if [ ".asm" != $(echo -n $1 | tail -c 4) ] && [ ".s" != $(echo -n $1 | tail -c 2) ]
 then
 	echo '''
@@ -8,3 +17,5 @@ Tenes que pasarme un archivo ".asm" o ".s"
 fi
 
 echo "Compilo el archivo asembly a objeto"
+
+
