3f57d4a1c30e01fa7540922448c6d2205ba4182c
Fabri <moboficial00@gmail.com>
2022-10-05 00:04:18 -0300

    Chequeo que el usuario realmente pase un archivo

--- compilame.sh
+++ compilame.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+if [ -z "$1" ]
+then
+	echo "
+Ojo, tenes que pasar un archivo como primer parametro para que el programa funcione.
+Ejemplo: compilame.sh hola_mundo.asm
+"
+	exit 2 #Paso 2 como error porque el usuario no paso los archivos necesarios
+fi
+
+
 if [ ! -f "$1" ]
 then
 	echo "
@@ -7,6 +17,7 @@ ${1} no existe
 	"
 	exit 2 #Paso 2 como error porque el usuario no paso un archivo existente
 fi
+
 
 if [ ".asm" != $(echo -n $1 | tail -c 4) ] && [ ".s" != $(echo -n $1 | tail -c 2) ]
 then
