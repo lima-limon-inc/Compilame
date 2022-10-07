bfa3f237894de11272b0f19a2ab84f99937c0d58
Fabri <moboficial00@gmail.com>
2022-10-07 15:06:24 -0300

    Primera build con las flags

--- compilame.sh
+++ compilame.sh
@@ -1,5 +1,31 @@
 #!/bin/sh
 #Compilame version 2.1
+debug=0 #Si debug = 0, entonces no corremos los comandos para debugeo. Si es igual a 1, si
+
+Help () {
+
+echo "
+Compilame. Programa 'wrapper' de los compiladores NASM y GCC.
+
+Formato de los comandos:
+compilame.sh -opciones archivo.asm
+
+Opciones disponibles: (En la version actual no se pueden combinar y usar mas de una)
+-h: Help, Ayuda
+-d: Debugear: Corre los comandos para poder debugear el codigo maquina con algun debuger (el gdb por ejemplo). ADVERTENCIA: ACTUALMENTE NO SE PUEDE DEBUGEAR. Si uno QUISIERA debugear tiene que recompilar el el compilador NASM. APARENTEMENTE, esta build del patcheada del NASM funciona . Tiene una version precompilada para distribuciones Debian o sino el codigo fuente para compilar manualmente. (DESCARGAR A RIESGO PROPIO): https://github.com/iglosiggio/nasm/releases
+"
+	exit 0 #Salgo deel programa porque el usuario pidio ver la ayuda
+}
+
+while getopts $opciones opt
+do
+	case "${opt}" in
+	d) debugeo=1 ;;
+	h | help ) Help ;;
+	\?) echo "Opcion desconocida $OPTARG"; exit 1 ;;
+	esac
+	shift
+done
 
 if [ -z "$1" ]
 then
